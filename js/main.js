126af5c2aee6ff1728ac7ef48a6cb46e27a82005
unknown <james.r.pratt41@gmail.com>
2019-03-11 01:19:06 -0400

    The drag feature isn't functioning like it was yesterday. The append fill is broken. But your designs and svgs are gorgeous.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-console.log("javascript linked up")
+console.log('fired');
+
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+// Fill listeners
+fill.addEventListener('dragend', dragEnd);
+
+// Loop through empty boxes and add listeners
+for (const empty of empties) {
+  empty.addEventListener('dragover', dragOver);
+
+  empty.addEventListener('drop', dragDrop);
+}
+
+// Drag Functions
+
+function dragEnd() {
+  this.className = 'fill';
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+
+
+function dragDrop() {
+  this.className = 'empty';
+  this.append(fill);
+}
